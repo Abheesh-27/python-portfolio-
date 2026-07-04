84d6b5dc93180819f80fb0f2a35c3496d31fa4c8
Abheesh <abheeshkousthubha02@gmail.com>
2026-07-04 20:58:50 +0530

    Day 2 - Operators

---
+++ 01_Python_Basics/day02_operators.py
@@ -0,0 +1,31 @@
+a = 20
+b = 6
+
+print("Addition: ", a+b)
+print("Subtraction: ", a-b)
+print("Multiplication: ", a*b)
+print("Division: ", a/b)
+print("Floor Division: ", a//b)
+print("Remainder: ", a%b)
+print("Exponenet: ", a**2)
+
+print()
+
+x = 10
+y = 15
+
+print("x == y:", x == y)
+print("x != y:", x != y)
+print("x > y:", x > y)
+print("x < y:", x < y)
+print("x >= y:", x >= y)
+print("x <= y:", x <= y)
+
+print()
+
+age = 20
+has_id = True
+
+print("Can enter:", age >= 18 and has_id)
+print("Eligible:", age >= 18 or has_id)
+print("Has ID:", not has_id)
