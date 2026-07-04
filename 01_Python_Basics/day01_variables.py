99b32511ba4c8b10532e26e169844493c8dc40d0
Abheesh <abheeshkousthubha02@gmail.com>
2026-07-04 20:50:09 +0530

    Day 1 - Variables and Data Types

---
+++ 01_Python_Basics/day01_variables.py
@@ -0,0 +1,15 @@
+print("Learning Python")
+
+name = 'M. Abheesh Kousthubha'
+age = 20
+college = 'IIT Madras'
+
+cgpa = 6.38
+is_student = True
+
+print(name)
+print(age)
+print(college)
+print(cgpa)
+print(is_student)
+
